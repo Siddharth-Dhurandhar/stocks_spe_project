6b53abc6c8523b30541113f179427fb41427d4e2
Gaurav Verma <77579876+Frost-04@users.noreply.github.com>
2025-05-02 18:47:14 +0530

    Backend base

---
+++ Backend/stock_input_monitor/src/main/java/com/gaurav/microservices/stock_input_monitor/StockInputMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.microservices.stock_input_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInputMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockInputMonitorApplication.class, args);
+    }
+
+}
---
+++ Backend/stock_input_monitor/src/test/java/com/gaurav/microservices/stock_input_monitor/StockInputMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.microservices.stock_input_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInputMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
