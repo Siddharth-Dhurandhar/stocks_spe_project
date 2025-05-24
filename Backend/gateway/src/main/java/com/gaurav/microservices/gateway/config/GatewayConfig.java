47cdd1fe7fbe6d058b7b0c51a9b7de45cbe1fa65
Gaurav Verma <77579876+Frost-04@users.noreply.github.com>
2025-05-24 16:28:37 +0530

    minor changes

--- Backend/gateway/src/main/java/com/gaurav/microservices/gateway/config/GatewayConfig.java
+++ Backend/gateway/src/main/java/com/gaurav/microservices/gateway/config/GatewayConfig.java
@@ -11,9 +11,29 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("actuator-routes", r -> r
-                        .path("/*/actuator/**")
-                        .uri("lb://#{segment[0]}")
+                .route("gateway-actuator", r -> r
+                        .path("/gateway/actuator/**")
+                        .uri("lb://gateway")
+                )
+                .route("stock-ingestion-actuator", r -> r
+                        .path("/stock-ingestion/actuator/**")
+                        .uri("lb://stock-ingestion")
+                )
+                .route("price-monitor-actuator", r -> r
+                        .path("/price-monitor/actuator/**")
+                        .uri("lb://price-monitor")
+                )
+                .route("signup-actuator", r -> r
+                        .path("/signup/actuator/**")
+                        .uri("lb://signup")
+                )
+                .route("user-activity-actuator", r -> r
+                        .path("/user-activity/actuator/**")
+                        .uri("lb://user-activity")
+                )
+                .route("outputmonitor-actuator", r -> r
+                        .path("/outputmonitor/actuator/**")
+                        .uri("lb://outputmonitor")
                 )
                 .build();
     }
